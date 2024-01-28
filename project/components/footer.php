b03598995ff3b88317324b826c23385df7009397
AKASH VISHWAKARMA <122552889+akash9198@users.noreply.github.com>
2024-01-28 19:44:13 +0530

    Add files via upload

---
+++ project/components/footer.php
@@ -0,0 +1,26 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+   <section class="flex">
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i><span>123456789</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>6387442953</span></a>
+         <a href="mailto:shaikhanas@gmail.com"><i class="fas fa-envelope"></i><span>akv6495@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>MMMUT, Gorakhpur, Bharat - 270310</span></a>
+      </div>
+      <div class="box">
+         <a href="home.php"><span>Home</span></a>
+         <a href="about.php"><span>About</span></a>
+         <a href="contact.php"><span>Contact</span></a>
+         <a href="listings.php"><span>All listings</span></a>
+         <a href="saved.php"><span>Saved properties</span></a>
+      </div>
+      <div class="box">
+         <a href="#"><span>Facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>Twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="https://www.linkedin.com/in/akash-vishwakarma-48495525a/"><span>Linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>Instagram</span><i class="fab fa-instagram"></i></a>
+      </div>
+   </section>
+   <div class="credit">&copy; Copyright @ <?= date('Y'); ?> by <span>Akash</span> | All Rights Reserved!</div>
+</footer>
